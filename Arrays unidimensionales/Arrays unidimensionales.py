284aacfb1e1211e46d7223841ca1c26e855416ca
leanxxi <leandrorobles17@gmail.comgit config user.name leanxxigit config user.email leandrorobles17@gmail.com>
2024-05-11 17:08:50 -0300

    resolviendo arrays unidimensionales

--- Arrays unidimensionales/Arrays unidimensionales.py
+++ Arrays unidimensionales/Arrays unidimensionales.py
@@ -14,36 +14,57 @@
 # reemplazo y luego retornar la cantidad total de reemplazos realizados.
 
 from funcionesArrays import *
+import os
 
 
-
-
+lista_de_nombres = ["Javier", "Luis", "Jose", "Ramiro", "Guillermo", "Javier"]
 lista_creada = crear_lista_de_numeros()
 
-
-
-
-print(f"promedio positivos = {calcular_promedio_positivos(lista_creada)}")
-
 while True:
-    opcion = input("ingrese opcion")
+    print("""Bienvenido al Menu
+          1-calcular promedio de la lista
+          2-calcular promedio de los positivos de la lista
+          3-calcular producto de todos los elementos de la lista
+          4-retornar la posicion del valor maximo de la lista
+          5-encontrar la posicion del valor maximo de la lista
+          6-reemplazar nombre de una lista y retornar la cantidad total de reemplazos realizados.
+          7-salir
+
+""")
+
+    opcion = ingresar_entero('ingrese opcion')
 
     match opcion:   
         case 1:
             #1. Escribir una función que reciba una lista de enteros, la misma calculará y devolverá el
             # promedio de todos los números.
-            pass
+            print(f"promedio = {calcular_el_promedio_de_listas(lista_creada)}")            
         case 2:
-            pass
+            # 2. Escribir una función parecida a la anterior, pero la misma deberá calcular y devolver el
+            # promedio de los números positivos.
+            print(f"promedio positivos = {calcular_promedio_positivos(lista_creada)}")
         case 3:
-            pass
+            # 3. Escribir una función que calcule y retorne el producto de todos los elementos de la lista
+            # que recibe como parámetro.
+            print(f"producto = {calcular_producto_de_todos_los_elementos_de_la_lista(lista_creada)}")
         case 4:
-            pass
+            # 4. Escribir una función que reciba como parámetros una lista de enteros y retorne la posición
+            # del valor máximo encontrado.  
+            print(f"posicion = {encontrar_max_min_posc_lista(lista_creada,'posicion_maximo')}")
         case 5:
-            pass
+            # 5. Escribir una función que reciba como parámetros una lista de enteros y muestre la/las
+            # posiciones en donde se encuentra el valor máximo hallado.
+            print(f"posicion de los valores maximos = {encontrar_pos_maximos(lista_creada)}")
         case 6:
-            pass
+            #6. Escribe una función llamada reemplazar_nombres que reciba como parámetros una lista
+            # de nombres, un nombre a reemplazar y su correspondiente reemplazo. La función debe
+            # reemplazar cada ocurrencia del nombre a reemplazar en la lista con su correspondiente
+            # reemplazo y luego retornar la cantidad total de reemplazos realizados.
+
+            print(f"promedio positivos = {calcular_promedio_positivos(lista_creada)}")
         case 7:
             break
         case _:
             print("error: opcion inesperada")
+    os.system("pause")
+    os.system("cls")
--- Arrays unidimensionales/funcionesArrays.py
+++ Arrays unidimensionales/funcionesArrays.py
@@ -9,10 +9,11 @@ def ingresar_entero(mensaje = "ingrese entero")->int:
         entero = input(mensaje)
         if entero.isdigit():
             numero = int(entero)
-            print("El número ingresado es:", numero)
+            print("valor ingresado:", numero)
             return numero
         else:
             print("Error: La entrada no es un número.")
+
 
 def ingresar_flotante()->float:
     """ingresa un caracter, intenta cambiarlo a un numero flotante, si tiene exito lo devuelve y rompe el bucle
@@ -31,8 +32,12 @@ def ingresar_flotante()->float:
             # Si falla la conversión, muestra un mensaje y continúa el bucle
             print("Error: El dato ingresado no es un número flotante. Inténtelo de nuevo.")
 
+def mostrar_datos(lista : list)-> None:
+    for elemento in lista:
+        print(elemento)
 
-
+# lista = [1,3,4,5,6,7,8,9,10,11,12]
+# mostrar_datos(lista)
 
 def crear_lista_de_numeros()->list:
     """crea una lista vacia, en la cual se le pueden ir agregando solamente numeros
@@ -78,13 +83,13 @@ def calcular_el_promedio_de_listas(lista_de_numeros : list)->float :
 #ejercio 2
 
 def calcular_promedio_positivos(lista_de_numeros : list)->float :
-    """_summary_
+    """calcula el promedio pero solo de los numeros positivos sin incluir el 0
 
     Args:
-        lista_de_numeros (list): _description_
+        lista_de_numeros (list): lista de numeros ingresada
 
     Returns:
-        float: _description_
+        float: promedio de los numeros positivos de la lista
     """
     suma_de_lista = 0
     for i in range(len(lista_de_numeros)):
@@ -93,3 +98,100 @@ def calcular_promedio_positivos(lista_de_numeros : list)->float :
     
     promedio = suma_de_lista / len(lista_de_numeros)
     return promedio
+#ejercicio 3
+ # 3. Escribir una función que calcule y retorne el producto de todos los elementos de la lista
+# que recibe como parámetro.
+def calcular_producto_de_todos_los_elementos_de_la_lista(lista_de_numeros : list)->float:
+    """calcula el producto de todos los elementos de la lista
+    Args:
+        lista_de_numeros (list): lista de números
+    Returns:
+        float: producto de todos los elementos de la lista
+    """
+    producto_de_lista = 1
+    for elemento in lista_de_numeros:
+        producto_de_lista *= elemento
+    
+    return producto_de_lista
+
+#ejercicio 4
+# 4. Escribir una función que reciba como parámetros una lista de enteros y retorne la posición
+# del valor máximo encontrado.  
+
+def encontrar_max_min_posc_lista(lista_de_numeros : list, maximo_minimo_posicion = "maximo")-> float |int:
+    """
+    Ecuentra el maximo, el minimo o la posicion de los mismos segun el parametro
+    Args:
+        lista_de_numeros (list):lista de numeros
+        
+        maximo_minimo_posicion (str, optional): "maximo" = devuelve el valor maximo en la lista. el argumento por defecto es "maximo".
+            Returns:
+                float: valor maximo 
+        
+        maximo_minimo_posicion (str, optional):"minimo" devuelve el valor minimo de la lista
+            Returns:
+                float: valor minimo        
+        maximo_minimo_posicion (str, optional):"posicion_minimo": devuelve la posicion del valor minimo en la lista. 
+            Returns:
+                int: posicion del valor minimo
+        
+        maximo_minimo_posicion (str, optional):"posicion_maximo": devuelve la posicion del valor maximo en la lista 
+            Returns:
+                int: posicion del valor maximo 
+                  """
+    for i in range(len(lista_de_numeros)):
+        if i == 0:
+            maximo = lista_de_numeros[i]
+            minimo = lista_de_numeros[i]
+            posicion_maximo = 0
+            posicion_minimo = 0
+        elif lista_de_numeros[i] > maximo:
+            maximo = lista_de_numeros[i]
+            posicion_maximo = i
+        elif lista_de_numeros[i] < minimo:
+            posicion_minimo = i
+            minimo = lista_de_numeros[i]
+
+    if maximo_minimo_posicion == "maximo":
+        return maximo
+    elif maximo_minimo_posicion == "minimo":
+        return minimo
+    elif maximo_minimo_posicion == "posicion_maximo":
+        return posicion_maximo
+    elif maximo_minimo_posicion == "posicion_minimo":
+        return posicion_minimo
+    else:
+        print("error: argumento invalido")
+
+#ejercicio 5
+# 5. Escribir una función que reciba como parámetros una lista de enteros y muestre la/las
+# posiciones en donde se encuentra el valor máximo hallado.
+
+def encontrar_pos_maximos(lista_de_numeros:list):
+    """devuelve una lista con la poscicion de los valores maximos
+
+    Args:
+        lista_de_numeros (list): lista de numeros
+
+    Returns:
+        list: posicion de los valores maximos
+    """
+    maximo = encontrar_max_min_posc_lista(lista_de_numeros)
+    maximos_posicion = []
+    for i in range(len(lista_de_numeros)):
+        if lista_de_numeros[i] == maximo:
+            maximos_posicion.append(i)
+    return maximos_posicion
+
+# 6. Escribe una función llamada reemplazar_nombres que reciba como parámetros una lista
+# de nombres, un nombre a reemplazar y su correspondiente reemplazo. La función debe
+# reemplazar cada ocurrencia del nombre a reemplazar en la lista con su correspondiente
+# reemplazo y luego retornar la cantidad total de reemplazos realizados.
+
+def reemplazar_nombres(lista_de_nombres : list, nombre_a_reemplazar, reemplazo)->int:
+    for nombre in lista_de_nombres:
+        if nombre == nombre_a_reemplazar:
+            nomb
+
+
+
