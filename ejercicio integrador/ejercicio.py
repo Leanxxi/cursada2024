e04faeb86502b17091b2d8392e131407621f44e5
leanxxi <leandrorobles17@gmail.comgit config user.name leanxxigit config user.email leandrorobles17@gmail.com>
2024-05-18 18:37:43 -0300

    haciendo ejercicio integral

--- ejercicio integrador/ejercicio.py
+++ ejercicio integrador/ejercicio.py
@@ -14,38 +14,7 @@
 # ● Calcular el día con la mayor variación en los ingresos respecto al día anterior.
 # 3. Muestra de datos: Una vez calculados todos los datos del punto 2, se deberán
 # imprimir en pantalla
-from funciones import (ingresar_entero)
-import os
+
 
 lista_dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
 lista_ingresos = [0,0,0,0,0,0,0]
-
-
-while True:
-    print("""       Bienvenido al Menu
-        1-Registrar ingresos
-        2-Analizar datos
-        3-Mostrar todos los datos
-        4-salir
-
-""")
-    opcion = ingresar_entero('ingrese opcion')
-    match opcion:   
-        case 1:
-            pass          
-        case 2:
-            pass
-        case 3:
-            pass
-        case 4:
-            pass                  
-        case 5:
-            pass
-        case 6:
-            pass
-        case 7:
-            break
-        case _:
-            print("error: opcion inesperada")
-    os.system("pause")
-    os.system("cls")
--- ejercicio integrador/funciones.py
+++ ejercicio integrador/funciones.py
@@ -14,6 +14,10 @@
 # ● Calcular el día con la mayor variación en los ingresos respecto al día anterior.
 # 3. Muestra de datos: Una vez calculados todos los datos del punto 2, se deberán
 # imprimir en pantalla
+import os
+
+lista_dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+lista_ingresos = [0,0,0,0,0,0,0]
 
 def ingresar_entero(mensaje = "ingrese entero")->int:
     """ingresa un numero entero, lo valida y lo devuelve
@@ -48,3 +52,76 @@ def ingresar_flotante()->float:
         except ValueError:
             # Si falla la conversión, muestra un mensaje y continúa el bucle
             print("Error: El dato ingresado no es un número flotante. Inténtelo de nuevo.")
+
+def mostrar_menu_principal():
+    print("""       Bienvenido al Menu
+            1-Registrar ingresos
+            2-Analizar datos
+            3-Mostrar todos los datos
+            4-salir
+
+    """)
+
+def menu_principal():
+    while True:
+        
+        opcion = ingresar_entero('ingrese opcion')
+        match opcion: 
+            case 1:
+                
+                pass          
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass                  
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                break
+            case _:
+                print("error: opcion inesperada")
+        os.system("pause")
+        os.system("cls")
+
+def mostrar_submenu_ingresos():
+    print("""
+----------------Menu de registros de ingresos----------------
+        1-Registrar Lunes
+        2-Registrar Martes
+        3-Registrar Miercoles
+        4-Registrar Jueves
+        5-Registrar Viernes
+        6-Registrar Sabados
+        7-Registrar Domingos
+        8-Volver al menu principal  
+
+""")
+
+def sub_menu_ingresos():
+    mostrar_submenu_ingresos()
+    while True:
+        opcion = input("Ingrese Opcion")
+
+        match opcion:
+            case '1':
+                pass
+            case '2':
+                pass
+            case '3':
+                pass
+            case '4':
+                pass
+            case '5':
+                pass
+            case '6':
+                pass
+            case '7':
+                pass
+            case '8':
+                break
+
+def registros_semanales(lista_de_semana):
